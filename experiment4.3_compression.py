cb949fb69594a8d7aa5fa1c557d9f5843776a496
johnfouf <johnfouf@gmail.com>
2021-12-10 13:15:42 +0200

    add experiments

--- experiment4.3_compression.py
+++ experiment4.3_compression.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 import pandas as pd
-df = pd.read_csv("/home/openaire/phd/datasets/edgar/ips.csv") # edit with csv input file path
+df = pd.read_csv("reprod_datasets/edgar/ips.csv") # edit with csv input file path
 write("ips.diff", df, row_group_offsets=65535, format="ADAPTIVE") 
 write("ips.localdiff", df, row_group_offsets=65535, format="LOCAL")
 global_encoding.write_global(df,"ips.unorderedglob",0,65535)
