840f4f21cfcf89e7371e65fbf35131c991a722b3
johnfouf <johnfouf@gmail.com>
2021-12-10 13:20:36 +0200

    Update experiment4.4_compress_python.py

--- experiment4.4_compress_python.py
+++ experiment4.4_compress_python.py
@@ -2,11 +2,11 @@ from pyimplementation.fastparquet import write
 import pandas as pd
 import timeit
 print('Compressing using parquet techniques at: ',timeit.timeit('''
-df = pd.read_csv("/home/openaire/arc/arcade/src/citationspart.0.csv", usecols = ['c1'])
+df = pd.read_csv("reprod_datasets/citations/citationspart.0.csv", usecols = ['c1'])
 write("citations.parq", df, row_group_offsets=65535)
 ''', globals=globals(), number=1))
 print('Compressing using adaptive dictionaries at: ',timeit.timeit('''
-df1 = pd.read_csv("/home/openaire/arc/arcade/src/citationspart.0.csv", usecols = ['c1'])
+df1 = pd.read_csv("reprod_datasets/citations/citationspart.0.csv", usecols = ['c1'])
 write("citations.diffparq", df1, row_group_offsets=65535, format = "ADAPTIVE")
 ''', globals=globals(), number=1))
 
